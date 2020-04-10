858b4fa693215f3621fedfc8f2bff5c5d53121fc
Dominic Montelongo <padma@email.com>
2020-04-10 10:34:06 -0700

    Add pizza constructor and brainstorm prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
+function Pizza (size, toppings) {
+    this.size = size;
+    this.toppings = toppings;
+}
+
+Pizza.prototype.createSize 
+
+Pizza.prototype.createToppings
+
 // User Interface Logic
+
+
